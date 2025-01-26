e40ee28ef45438dda798700b11861e622b4c48d6
Anton Arkhipov <antonolegovich44@yandex.ru>
2025-01-26 23:14:17 +0300

    fix model statuses

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,17 +1,20 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
 class Statuses(models.Model):
-    name = models.CharField('Имя', max_length=50, unique=True)
+    name = models.CharField("Имя", max_length=50, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
-    def delete(self, *args, **kwargs):
-        if self.task_set.all().count() != 0:
-            raise Exception('Нельзя удалять статус с задачами')
-        super().delete(*args, **kwargs)
+    # def delete(self, *args, **kwargs):
+    #     if self.task_set.exists():
+    #         raise ValidationError(
+    #             "Невозможно удалить статус, так как с ним связаны задачи."
+    #         )
+    #     super().delete(*args, **kwargs)
 
     class Meta:
         db_table = "statuses"
