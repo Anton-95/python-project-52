601d3cd215ec196566d8500b2b1a62c0b12b345f
Anton Arkhipov <antonolegovich44@yandex.ru>
2025-01-27 23:30:45 +0300

    feat: add task model

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+from task_manager.statuses.models import Status
+from task_manager.users.models import User
+
+
+class Task(models.Model):
+    name = models.CharField(verbose_name="Имя", max_length=100, unique=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        verbose_name="Автор",
+        related_name="tasks"
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+        blank=True,
+        null=True
+    )
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.PROTECT,
+        verbose_name="Статус",
+        related_name="tasks"
+    )
+    executor = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        verbose_name="Исполнитель",
+        related_name="task",
+        blank=True,
+        null=True
+    )
+    # label = models.ManyToManyField(
+    #     "Label",
+    #     verbose_name="Метки",
+    #     related_name="tasks"
+    # )
+    created_at = models.DateTimeField(auto_now_add=True)
