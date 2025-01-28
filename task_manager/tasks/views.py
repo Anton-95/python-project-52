995d0c308c7a954233215481dae427f2e083fb9c
Anton Arkhipov <antonolegovich44@yandex.ru>
2025-01-28 23:29:42 +0300

    feat: add views task list and create

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from task_manager.tasks.models import Task
+from django.contrib import messages
 
-# Create your views here.
+
+class TasksView(ListView):
+    model = Task
+    template_name = "tasks/tasks_list.html"
+    context_object_name = "tasks"
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    template_name = "tasks/tasks_create.html"
+    fields = ['name', 'description', 'status', 'executor']
+    success_url = reverse_lazy("tasks")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        messages.success(self.request, "Задача успешно создана")
+        return super().form_valid(form)
