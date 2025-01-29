31ef2f613fccfee23df472a93ccba622a61a16f2
Anton Arkhipov <antonolegovich44@yandex.ru>
2025-01-29 22:53:12 +0300

    fix: delete unused form

--- task_manager/statuses/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from task_manager.statuses.models import Status
-
-
-class StatusCreateForm(forms.ModelForm):
-    class Meta:
-        model = Status
-        fields = ["name"]
