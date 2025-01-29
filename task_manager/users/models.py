fa59a452eb62e1a4490cad54e9822f6ad1522335
Anton Arkhipov <antonolegovich44@yandex.ru>
2025-01-29 22:56:10 +0300

    feat: add str view model

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -4,3 +4,6 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     class Meta:
         db_table = "users"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
