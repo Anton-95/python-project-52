09f483fde46fc22c558a764ce82e8d23bcd334a8
Anton Arkhipov <antonolegovich44@yandex.ru>
2025-01-30 21:42:38 +0300

    feat: add label model

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Label(models.Model):
+    name = models.CharField("Имя", max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "labels"
