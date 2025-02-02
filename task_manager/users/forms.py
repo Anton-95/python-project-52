c9aa2e417ed43817fe10eeb12d57885e07142661
Anton Arkhipov <antonolegovich44@yandex.ru>
2025-02-02 18:50:14 +0300

    feat: add change user password

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
 from task_manager.users.models import User
 
 
@@ -9,7 +9,16 @@ class CustomUsersCreateForm(UserCreationForm):
         fields = ["first_name", "last_name", "username", "password1", "password2"]
 
 
-class CustomUsersUpdateForm(UserChangeForm):
-    class Meta:
-        model = User
-        fields = ["first_name", "last_name", "username", "password"]
+class CustomUsersUpdateForm(CustomUsersCreateForm):
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop("user", None)
+        super().__init__(*args, **kwargs)
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        if User.objects.filter(username=username).exists():
+            if self.user and self.user.username != username:
+                raise forms.ValidationError(
+                    "Пользователь с таким именем уже существует"
+                    )
+        return username
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -47,9 +47,21 @@ class UsersUpdateView(UpdateView):
             return redirect("users")
         return super().dispatch(request=request, *args, **kwargs)
 
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
     def form_valid(self, form):
         messages.success(self.request, "Пользователь успешно обновлен")
         return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(
+            self.request,
+            "Пользователь с таким именем уже существует"
+            )
+        return super().form_invalid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
