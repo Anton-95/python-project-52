935ab1d1d17dcf522d704426000d5138beb5227d
Anton Arkhipov <antonolegovich44@yandex.ru>
2025-02-05 22:31:04 +0300

    feat: add models to the admin panel

--- task_manager/tasks/admin.py
+++ task_manager/tasks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
 
-# Register your models here.
+from .models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "author", "executor", "status", "created_at")
+    search_fields = ["name"]
+    list_filter = [("created_at", DateFieldListFilter)]
